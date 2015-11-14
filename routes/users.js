552967b2533fb5b97f2b942ce5ce203aa2d89feb
ibelle <Isaiah.Belle@gmail.com>
2015-11-14 11:53:01 -0500

    Added Formating to Route
    Added Formating to Route
    
    Signed-off-by: ibelle <Isaiah.Belle@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var util = require('util');
 var router = express.Router();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  res.send( util.format('respond with a resource %j',req.query));
 });
 
 module.exports = router;
